db5dd849e43366e5c90c0aff9fbdef5bb47c3e56
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2025-09-05 20:03:00 +0500

    tab finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,14 +69,62 @@ const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute("aria-expanded");
-
+  
   for (i = 0; i < items.length; i++) {
     items[i].setAttribute("aria-expanded", "false");
   }
-
+  
   if (itemToggle == "false") {
     this.setAttribute("aria-expanded", "true");
   }
 }
 
 items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+// ============================================================================
+// TEAM MODAL (TABS)
+// Табы
+    const tabs = document.querySelectorAll(".tab");
+    const cards = document.querySelectorAll(".card");
+
+    tabs.forEach(tab => {
+      tab.addEventListener("click", () => {
+        tabs.forEach(t => t.classList.remove("active"));
+        tab.classList.add("active");
+        const filter = tab.getAttribute("data-filter");
+
+        cards.forEach(card => {
+          if (filter === "all" || card.dataset.category === filter) {
+            card.style.display = "block";
+          } else {
+            card.style.display = "none";
+          }
+        });
+      });
+    });
+
+    // Модалка
+    const teamModal = document.getElementById("teamModal");
+    const teamModalImg = document.getElementById("teamModalImg");
+    const teamModalName = document.getElementById("teamModalName");
+    const teamModalRole = document.getElementById("teamModalRole");
+    const teamCloseBtn = document.getElementById("teamCloseBtn");
+
+    cards.forEach(card => {
+      card.addEventListener("click", () => {
+        teamModal.style.display = "flex";
+        teamModalImg.src = card.dataset.img;
+        teamModalName.textContent = card.dataset.name;
+        teamModalRole.textContent = card.dataset.role;
+      });
+    });
+
+    teamCloseBtn.addEventListener("click", () => {
+      teamModal.style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+      if (e.target === teamModal) {
+        teamModal.style.display = "none";
+      }
+    });
